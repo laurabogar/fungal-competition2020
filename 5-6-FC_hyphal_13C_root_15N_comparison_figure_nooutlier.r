e75f4fb106b0d4c6d3afbcf6c8b032904debe11b
Laura Bogar <lbogar@stanford.edu>
2020-10-21 12:26:04 -0700

    Discovered ggPredict and used to understand my models much better. Must switch to using only root 15N as a 15N measure from now on.

--- 5-6-FC_hyphal_13C_root_15N_comparison_figure_nooutlier.r
+++ 5-6-FC_hyphal_13C_root_15N_comparison_figure_nooutlier.r
@@ -5,6 +5,7 @@
 library(cowplot)
 library(tidyverse)
 library(stargazer)
+library(ggiraphExtra) # required for ggpredict() visualization.
 
 carboninfo = read_csv("processeddata/data_for_carbon_only_analyses.csv")
 nitrogeninfo = read_csv("processeddata/isotope_and_plant_metadata_FOR_N_ANALYSES_and_exchange_rates.csv")
@@ -103,20 +104,22 @@ save_plot("plots/Multipanel_regressions_myco_N_and_C_NOOUTLIER.pdf",
 
 ## Adding fourth panel? ####
 data_for_hypharootplot = nitrogeninfo_nooutlier %>% drop_na(hyphae.ppm13Cexcess)
+data_for_hypharootplot_justTt = subset(data_for_hypharootplot,
+                                       compartment_fungus == "Tt")
 # I have literally one Suillus compartment in here.
 # Can't analyze effect of fungus, so best to exclude it.
 
 # data_for_hypharootplot = subset(data_for_hypharootplot, compartment_fungus != "Sp")
+data_for_hypharootplot$hyphalog13C = log(data_for_hypharootplot$hyphae.ppm13Cexcess)
 
 
-hyphaCforrootN = ggplot(data = data_for_hypharootplot) +
-  geom_point(aes(y = log(hyphae.ppm13Cexcess),
+hyphaCforrootN = ggplot(data = data_for_hypharootplot_justTt) +
+  geom_point(aes(y = hyphalog13C,
                  x = nmlogN15, 
-                 color = N_level,
-                 shape = compartment_fungus)) +
+                 color = N_level)) +
   geom_smooth(method = "lm", 
               formula = y ~ x, 
-              aes(y = log(hyphae.ppm13Cexcess),
+              aes(y = hyphalog13C,
                   x = nmlogN15),
               color = "black",
               size = 0.5) +
@@ -124,13 +127,28 @@ hyphaCforrootN = ggplot(data = data_for_hypharootplot) +
   xlab(bquote(atop("Root "^15*"N (ln ppm excess)"))) +
   scale_color_manual(values = c("steelblue4", "steelblue1"),
                      name = "N level") +
-  scale_shape_manual(values = c(17, 15),
-                     name = "Fungus") +
+  # scale_shape_manual(values = c(17, 15),
+  #                    name = "Fungus") +
   theme(plot.margin = unit(c(1,1,1,1), "cm"))
 
-model_hyphaerootCforN_nooutlier = lm(log(hyphae.ppm13Cexcess) ~ nmlogN15*N_level, data = data_for_hypharootplot)
+
+
+model_hyphaerootCforN_justTt = lm(hyphalog13C ~ nmlogN15*N_level, data = data_for_hypharootplot_justTt)
 summary(model_hyphaerootCforN_nooutlier)
-plot(model_hyphaerootCforN_nooutlier)
+
+ggPredict(model_hyphaerootCforN_justTt)
+# This web page is very helpful for thinking about slope differences:
+# https://blog.minitab.com/blog/adventures-in-statistics-2/how-to-compare-regression-lines-between-different-models
+# Looking at just Tt (and I think when I include the one Sp point),
+# the intercept is almost significant (p = 0.06) and
+# the slope with nmlogn15 is almost significant (p = 0.0538),
+# but N level and its interaction with N15 are not.
+# I think this means I should include only the significant slope in my figure?
+
+model_HRCforN_justTt_noN = lm(hyphalog13C ~ nmlogN15, data = data_for_hypharootplot_justTt)
+summary(model_HRCforN_justTt_noN) # Huh, it's a much less predictive
+
+anova(model_hyphaerootCforN_nooutlier)
 
 model_hyphaerootCforN = lm(log(hyphae.ppm13Cexcess) ~ nmlogN15*N_level, data = nitrogeninfo)
 summary(model_hyphaerootCforN) # Literally the same
@@ -140,8 +158,22 @@ summary(model_hyphaerootCforN) # Literally the same
 mytest = lm(log(hyphae.ppm13Cexcess) ~ nmlogN15, data = data_for_hypharootplot)
 summary(mytest) # definitely worse to exclude N level here.
 
-anothermodel = lm(mycologC13 ~ mycologN15*N_level*compartment_fungus, data = nitrogeninfo_nooutlier)
-summary(anothermodel)
+fullmycomodel = lm(mycologC13 ~ mycologN15*N_level*compartment_fungus, data = nitrogeninfo_nooutlier)
+summary(fullmycomodel)
+anova(fullmycomodel)
+
+test = lm(mycologC13 ~ mycologN15, data = nitrogeninfo_nooutlier)
+summary(test)
+
+justTt = lm(mycologC13 ~ mycologN15*N_level, 
+            data = subset(nitrogeninfo_nooutlier, compartment_fungus == "Tt"))
+summary(justTt)
+anova(justTt)
+
+justSp = lm(mycologC13 ~ mycologN15*N_level, 
+            data = subset(nitrogeninfo_nooutlier, compartment_fungus == "Sp"))
+summary(justSp)
+anova(justSp)
 
 sink("stats_tables/hyphae13C_vs_root_15N_lm_loglogresults.html")
 
@@ -153,6 +185,135 @@ stargazer(model_hyphaerootCforN_nooutlier, type = "html",
           report = 'vc*s*p')
 
 sink()
+
+# How does the relationship change if we look
+# just at mycorrhizas in this same, reduced dataset?
+
+#### C for N changes a lot depending on N level and tissue ####
+model_justmycos = lm(mycologC13 ~ mycologN15*N_level, data = data_for_hypharootplot_justTt)
+summary(model_justmycos)
+anova(model_justmycos)
+ggPredict(model_justmycos, interactive = TRUE)
+ggPredict(model_hyphaerootCforN_justTt, interactive = TRUE)
+
+model_mycosC_rootsN = lm(mycologC13 ~ nmlogN15*N_level, data = data_for_hypharootplot_justTt)
+ggPredict(model_mycosC_rootsN, interactive = TRUE)
+
+
+
+mycoCformycoN_directcomp = ggplot(data = data_for_hypharootplot) +
+  geom_point(aes(y = mycologC13,
+                 x = mycologN15, 
+                 color = N_level,
+                 shape = compartment_fungus)) +
+  geom_smooth(method = "lm", 
+              formula = y ~ x, 
+              aes(y = mycologC13,
+                  x = mycologN15),
+              color = "black",
+              size = 0.5) +
+  ylab(bquote(atop("Mycorrhiza "^13*"C", "(ln ppm excess)"))) +
+  xlab(bquote(atop("Mycorrhiza "^15*"N (ln ppm excess)"))) +
+  scale_color_manual(values = c("steelblue4", "steelblue1"),
+                     name = "N level") +
+  scale_shape_manual(values = c(17, 15),
+                     name = "Fungus") +
+  theme(plot.margin = unit(c(1,1,1,1), "cm"))
+
+mycoCforN_justSp = ggplot(data = subset(nitrogeninfo_nooutlier,
+                                        compartment_fungus == "Sp")) +
+  geom_point(aes(x = mycologN15,
+                 y = mycologC13, 
+                 color = N_level,
+                 shape = compartment_fungus)) +
+  geom_smooth(method = "lm", 
+              formula = y ~ x, 
+              aes(x = mycologN15,
+                  y = mycologC13),
+              color = "black",
+              size = 0.5) +
+  ylab(bquote(atop("Mycorrhizal "^13*"C", "(ln ppm excess)"))) +
+  xlab(bquote(atop("Mycorrhizal "^15*"N (ln ppm excess)"))) +
+  scale_color_manual(values = c("steelblue4", "steelblue1"),
+                     name = "N level") +
+  scale_shape_manual(values = c(17, 15),
+                     name = "Fungus") +
+  theme(plot.margin = unit(c(1,1,1,1), "cm"))
+
+mycoCforN_justTt = ggplot(data = subset(nitrogeninfo_nooutlier,
+                                        compartment_fungus == "Tt")) +
+  geom_point(aes(x = mycologN15,
+                 y = mycologC13, 
+                 color = N_level,
+                 shape = compartment_fungus)) +
+  geom_smooth(method = "lm", 
+              formula = y ~ x, 
+              aes(x = mycologN15,
+                  y = mycologC13),
+              color = "black",
+              size = 0.5) +
+  ylab(bquote(atop("Mycorrhizal "^13*"C", "(ln ppm excess)"))) +
+  xlab(bquote(atop("Mycorrhizal "^15*"N (ln ppm excess)"))) +
+  scale_color_manual(values = c("steelblue4", "steelblue1"),
+                     name = "N level") +
+  scale_shape_manual(values = c(17, 15),
+                     name = "Fungus") +
+  theme(plot.margin = unit(c(1,1,1,1), "cm"))
+
+
+#### Recoloring the myco C for N figure to accommodate different slopes for Sp and Tt ####
+summary(fullmycomodel)
+# From what I can piece together here https://cran.r-project.org/web/packages/ggiraphExtra/vignettes/ggPredict.html,
+# the model summary should allow me to figure out the equations
+# for Sp and Tt.
+
+# Tt 13C = -0.8718 + 2.2295 (myco15N) +
+equation1=function(x){coef(fullmycomodel)[2]*x+coef(fit1)[1]}
+
+alternativemodel = lm(mycologC13 ~ mycologN15 * compartment_fungus * N_level,
+                      data = nitrogeninfo)
+
+ggPredict(alternativemodel, interactive = TRUE) # This is very cool, but I am not super
+# interested
+# WOW the interactive model is amazing!!!
+
+ggPredict(alternativemodel)
+
+fullmodel_rootsasN = lm(mycologC13 ~ nmlogN15 * compartment_fungus * N_level,
+                        data = nitrogeninfo)
+ggPredict(fullmodel_rootsasN)
+
+fullmodel_flippingaxes = lm(nmlogN15 ~ mycologC13 * compartment_fungus * N_level,
+                        data = nitrogeninfo)
+ggPredict(fullmodel_flippingaxes)
+
+
+mycoCforN_recolored = ggplot(data = nitrogeninfo_nooutlier) +
+  geom_point(aes(x = mycologN15,
+                 y = mycologC13, 
+                 shape = N_level,
+                 color = compartment_fungus)) +
+  geom_smooth(method = "lm", 
+              formula = y ~ x, 
+              aes(x = mycologN15,
+                  y = mycologC13),
+              color = "black",
+              size = 0.5) +
+  geom_smooth(data = subset(nitrogeninfo_nooutlier, 
+                            compartment_fungus == "Tt"),
+              method = "lm", 
+              formula = y ~ x, 
+              aes(x = mycologN15,
+                  y = mycologC13),
+              color = "orchid4",
+              size = 0.5) +
+  ylab(bquote(atop("Mycorrhizal "^13*"C", "(ln ppm excess)"))) +
+  xlab(bquote(atop("Mycorrhizal "^15*"N (ln ppm excess)"))) +
+  scale_color_manual(values = c("thistle3", "orchid4"),
+                     name = "Fungus") +
+  scale_shape_manual(values = c(19, 1),
+                     name = "N level") +
+  theme(plot.margin = unit(c(1,1,1,1), "cm"))
 
 # CforNlm_nooutlier = lm((mycoC13ppmexcess) ~ mycoN15ppmexcess, data = nitrogeninfo_nooutlier)
 # plot(CforNlm_nooutlier)
